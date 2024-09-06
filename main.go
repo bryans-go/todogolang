ffaf3fd2b7a7fcb9d94b4685813f173fcf0f3809
bryans-go <sabrinabryan1990@gmail.com>
2024-09-06 15:32:04 +0000

    good

--- main.go
+++ main.go
@@ -1,23 +1,100 @@
 package main
 
 import (
-	"log"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/bryans-go/todogolang/model"
-	"github.com/bryans-go/todogolang/storage"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
+type Todo struct {
+	task   string
+	isDone bool
+}
+
+var todos []Todo
+
 func main() {
-	db, err := storage.NewFileStorage("todos.json")
-	if err != nil {
-		log.Fatalf("Failed to open storage: %v", err)
-	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("\n1. Add Todo")
+		fmt.Println("2. List Todos")
+		fmt.Println("3. Mark Todo as Done")
+		fmt.Println("4. Delete Todo")
+		fmt.Println("5. Exit")
 
-	m := model.NewModel(db)
-	p := tea.NewProgram(m)
+		fmt.Print("Enter your choice: ")
+		scanner.Scan()
+		choice := scanner.Text()
 
-	if err := p.Start(); err != nil {
-		log.Fatalf("Error starting the program: %v", err)
+		switch choice {
+		case "1":
+			addTodo()
+		case "2":
+			listTodos()
+		case "3":
+			markTodoDone()
+		case "4":
+			deleteTodo()
+		case "5":
+			fmt.Println("Goodbye!")
+			return
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
 	}
+}
+
+func addTodo() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter the task: ")
+	scanner.Scan()
+	task := scanner.Text()
+	todos = append(todos, Todo{task: task, isDone: false})
+	fmt.Println("Todo added!")
+}
+
+func listTodos() {
+	if len(todos) == 0 {
+		fmt.Println("No todos to show.")
+		return
+	}
+	fmt.Println("\nYour Todos:")
+	for i, todo := range todos {
+		status := " "
+		if todo.isDone {
+			status = "x"
+		}
+		fmt.Printf("[%d] [%s] %s\n", i+1, status, todo.task)
+	}
+}
+
+func markTodoDone() {
+	scanner := bufio.NewScanner(os.Stdin)
+	listTodos()
+	fmt.Print("Enter the number of the todo to mark as done: ")
+	scanner.Scan()
+	input := scanner.Text()
+	index, err := strconv.Atoi(input)
+	if err != nil || index < 1 || index > len(todos) {
+		fmt.Println("Invalid number.")
+		return
+	}
+	todos[index-1].isDone = true
+	fmt.Println("Todo marked as done!")
+}
+
+func deleteTodo() {
+	scanner := bufio.NewScanner(os.Stdin)
+	listTodos()
+	fmt.Print("Enter the number of the todo to delete: ")
+	scanner.Scan()
+	input := scanner.Text()
+	index, err := strconv.Atoi(input)
+	if err != nil || index < 1 || index > len(todos) {
+		fmt.Println("Invalid number.")
+		return
+	}
+	todos = append(todos[:index-1], todos[index:]...)
+	fmt.Println("Todo deleted!")
 }
